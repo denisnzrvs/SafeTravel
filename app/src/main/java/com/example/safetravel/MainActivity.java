2b884e47b8e0808e17ab6f50f9f102fd04fd9036
Denis Nazarovs <denis.nazarovs@va.lv>
2023-10-21 18:46:50 +0300

    Profile data writing implemented

--- app/src/main/java/com/example/safetravel/MainActivity.java
+++ app/src/main/java/com/example/safetravel/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.safetravel;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -37,12 +38,11 @@ public class MainActivity extends AppCompatActivity {
             createProfile.setVisibility(View.VISIBLE);
             fragmentContainer.setVisibility(View.GONE);
             Toast.makeText(this, "No profile found! Please, create a new one!", Toast.LENGTH_SHORT).show();
-
+            //Navigate to CreateProfileActivity if profile doesn't exist.
             createProfile.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    // Handle button click, e.g., navigate to a profile creation fragment
-                    Toast.makeText(MainActivity.this, "Button pressed!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, CreateProfile.class));
                 }
             });
         } else {
